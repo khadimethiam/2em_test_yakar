07efc79166a704f1b9d697e82afd7f2bb4bd413e
Abou Sow <asow19133@gmail.com>
2024-11-25 10:47:26 +0000

    profile modal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-
+import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
 import { DashboardSimpleComponent } from './dashboard-simple/dashboard-simple.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginCodeComponent } from './login-code/login-code.component';
 import { UpdateComponent } from './update/update.component';
+import { MyModalComponent } from './modals/my-modal/my-modal.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -13,6 +15,7 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'user/edit/:id', component: UpdateComponent },
+  { path: 'admin/:id', component: MyModalComponent },
   
 
   {
@@ -21,3 +24,9 @@ export const routes: Routes = [
   },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/modals/my-modal/my-modal.component.ts
@@ -0,0 +1,101 @@
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common'; // Importez CommonModule
+import { UtilisateurService } from '../../services/utilisateur.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-my-modal',
+  standalone: true,
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  templateUrl: './my-modal.component.html',
+  styleUrl: './my-modal.component.css'
+})
+export class MyModalComponent implements OnInit {
+  isModalOpen = false;
+  isEditing = false;
+  isLoading = false;
+  userForm: FormGroup;
+ 
+  userId: string;
+
+
+  
+  constructor(
+    private fb: FormBuilder,
+    private userService: UtilisateurService,
+    private route: ActivatedRoute,
+    private router: Router,
+
+    
+  ) {
+    // Configuration du formulaire avec validation
+    this.userForm = this.fb.group({
+      nom: ['', [Validators.required, Validators.pattern(/^(?!\s)(?!.*\s\s)[a-zA-Z\s]*$/)]],
+      prenom: ['', [Validators.required, Validators.pattern(/^(?!\s)(?!.*\s\s)[a-zA-Z\s]*$/)]],
+      email: ['', [Validators.required, Validators.email]],
+      telephone: ['', [Validators.required, Validators.pattern(/^(70|75|76|77|78)\d{7}$/)]],
+      photo: [null]
+      
+    });
+    this.userId = '';
+  }
+
+
+  ngOnInit() {
+    this.userId = this.route.snapshot.paramMap.get('id')!;
+    const user = this.userService.getUserById(this.userId);
+    this.userForm = this.fb.group({
+      prenom: ['', Validators.required],
+      nom: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      telephone: ['', Validators.required]
+    });
+
+    this.loadUser();
+    this.openModal();
+  }
+  
+  loadUser() {
+    this.isLoading = true;
+    this.userService.getUserById(this.userId).subscribe({
+      next: (user) => {
+        this.userForm.patchValue({
+          nom: user.nom,
+          prenom: user.prenom,
+          email: user.email,
+          telephone: user.numero_tel,
+          photo: user.photo,
+        });
+        this.isLoading = false;
+      },
+      error: (err) => {
+        console.error(err);
+        this.isLoading = false;
+      }
+    });
+  }
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.isEditing = false;
+    this.userForm.reset();
+  }
+
+  toggleEditing() {
+    this.isEditing = true;
+  }
+
+  saveChanges() {
+    if (this.userForm.valid) {
+      const formData = this.userForm.value;
+      this.userService.modifierUtilisateur(this.userId, formData);
+      this.isEditing = false;
+      this.closeModal();
+    }
+  }
+}
